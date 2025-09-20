4e698c0f3de061f06d4f955019ee16a01ec5456a
asg149 <asg149@pitt.edu>
2025-09-20 19:01:57 -0400

    m

--- server/index.js
+++ server/index.js
@@ -8,5 +8,5 @@ app.use(express.json());
 
 app.use('/auth', authRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
