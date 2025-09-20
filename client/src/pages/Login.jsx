73c123b8b850257775a18d7908fd1cba60cc189f
asg149 <asg149@pitt.edu>
2025-09-20 14:22:38 -0400

    quick

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Login Page</h1>
+      <p>Spotify login button will go here.</p>
+    </div>
+  );
+};
+
+export default Login;
