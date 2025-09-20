93a2cdf65ff81968786c8b35b5ba2ab1a6b2bc24
asg149 <asg149@pitt.edu>
2025-09-20 14:11:41 -0400

    local host works

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Spotify Run App</h1>
+      <p>If you see this, React is working!</p>
+    </div>
+  );
+};
+
+export default App;
