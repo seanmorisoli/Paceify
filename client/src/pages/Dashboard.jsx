4eb85c39bd8659dba6daf28aaf06fb7fb3aedbff
asg149 <asg149@pitt.edu>
2025-09-21 13:04:49 -0400

    Update Dashboard.jsx

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -63,7 +63,7 @@ const Dashboard = () => {
 
   const fetchPublicPlaylist = async () => {
     try {
-      const playlistId = '37i9dQZF1DXcBWIGoYBM5M'; // Example: Spotify "Today's Top Hits"
+      const playlistId = '7ccEEmvlr6JX2sakWg8Tqm?si=1OfDEuDfS9eIsJ383ze0nQ'
       const res = await fetch(`${API_BASE_URL}/public/playlist/${playlistId}`);
       const data = await res.json();
       setTracks(data.tracks); // use your existing state
