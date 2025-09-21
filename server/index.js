34998714a7c3341bcd772fe272627bb8e7ff883f
asg149 <asg149@pitt.edu>
2025-09-21 14:20:27 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import cors from 'cors';
-import authRoutes from './routes/auth.js';
-import playlistRoutes from './routes/playlists.js';
-import filterRoutes from './routes/filter.js';
+import authRouter from './routes/auth.js';
+import playlistRouter from './routes/playlists.js';
+import filterRouter from './routes/filter.js';
 
 const app = express();
 app.use(cors());
@@ -10,9 +10,9 @@ app.use(express.json());
 
 // Mount routes
 
-app.use('/auth', authRoutes);
-app.use('/playlists', playlistRoutes);
-app.use('/filter', filterRoutes);
+app.use('/auth', authRouter);
+app.use('/playlists', playlistRouter);
+app.use('/filter', filterRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
